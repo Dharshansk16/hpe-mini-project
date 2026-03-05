c128762987a19cd198b686bfee3cf2448f0e3f09
Dharshan S Kotian <dcodecraft@gmail.com>
2026-03-05 18:41:25 +0530

    feat: project setup, add github loader, jira loader

---
+++ app/loaders/github_loader.py
@@ -0,0 +1,43 @@
+from github import Github
+
+class GithubLoader:
+    def __init__(self,git_client, repo):
+        if not repo or not isinstance(repo ,str):
+            raise ValueError("GitHub repository name must be a non-empty string.")
+        
+        self.git_client = git_client
+        self.repo =repo
+
+    def fetch_files(self):
+        try:
+            repo =self.git_client.get_user().get_repo(self.repo)
+        except Exception as e:
+            raise ValueError(f" Failed to connect to github: {str(e)}")
+        
+        contents = repo.get_contents("")
+        
+
+        documents = []
+        while contents:
+            file_content = contents.pop(0)
+            if file_content.type=="dir":
+                contents.extend(repo.get_contents(file_content.path))
+            elif file_content.type=="file":
+                try:
+                    file_data = file_content.decoded_content.decode("utf-8")
+                    doc= {
+                        "content": file_data,
+                        "metadata": {
+                            "path": file_content.path,
+                            "filename": file_content.name,
+                        }
+                    }
+                    documents.append(doc)
+                except Exception as e:
+                    print(f"Failed to decode file {file_content.path}: {str(e)}")
+        return documents
+    
+
+    
+
+    
---
+++ app/loaders/jira_loader.py
@@ -0,0 +1,31 @@
+
+class JiraLoader:
+    def __init__(self, jira_client, project_key):
+        self.jira_client = jira_client
+        self.project_key = project_key
+
+    def fetch_issues(self):
+        try:
+            res = self.jira_client.jql(f'project = {self.project_key}')
+            issues = res.get('issues', [])
+            documents = []
+            for issue in issues:
+                fields = issue.get('fields', {})
+
+                description = fields.get('description', '')
+                summary = fields.get("summary", "No Summary")
+                full_content = f"Summary: {summary}\n\n Description: {description}"
+                doc = {
+                    "content": full_content,
+                    "metadata": {
+                        "key": issue.get("key"),
+                        "summary": summary,
+                        "status": fields.get("status", {}).get("name"),
+                        "assignee": fields.get("assignee", {}).get("displayName") if fields.get("assignee") else None,
+                    }
+                }
+                documents.append(doc)
+            return documents
+        except Exception as e:
+            raise ValueError(f"Failed to fetch issues from Jira: {str(e)}")
+        
